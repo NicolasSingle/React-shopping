655da9fa41e0c0c196a71c70650b3219e06b6431
yzbgyq <710851698@qq.com>
2019-05-15 22:57:28 +0800

    4

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Button} from 'antd-mobile';
+function App() {
+    return (
+        <div>
+            <Button>default</Button>
+        </div>
+    );
+}
+
+export default App;
