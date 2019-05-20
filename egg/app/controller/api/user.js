40b99cfc4f755227390ae705cdf18affafc675ac
yzbgyq <710851698@qq.com>
2019-05-20 09:36:36 +0800

    完成登录

--- egg/app/controller/api/user.js
+++ egg/app/controller/api/user.js
@@ -6,6 +6,28 @@ class UserController extends BaseController {
         // const token = await this.service.token.setToken(userToken)
         // // const aa = await this.app.jwt.verify(token, this.app.config.secret)
         // console.log(token);
+        const { ctx } = this
+        const { username, password, verify } = ctx.request.body
+        if (!username || !password) {
+            return this.error('请输入完整信息')
+        }
+        // if (this.ctx.session.code.toUpperCase() !== verify.toUpperCase()) {
+        //     return ctx.body = {
+        //         code: -2,
+        //         msg: '验证码错误'
+        //     }
+        // }
+        let data = await ctx.model.User.findOne({ username })
+        if (!data || !data.username) {  //说明数据库没有这个名字
+            this.error('用户名或邮箱错误')
+        } else {
+            if (data.password != md5(password)) {
+                this.error('密码错误')
+            } else {
+                const token = await this.service.token.setToken(user.username)
+                this.success('登录成功', token)
+            }
+        }
     }
 
     async register() {
@@ -27,8 +49,7 @@ class UserController extends BaseController {
             let user = await new ctx.model.User(ctx.request.body)
             await user.save()
             // Token
-            const token = await this.service.token.setToken(user.username)
-            this.success('注册成功', token)
+            
         } else {
             if (data && data.username === username) return this.error('用户名已存在')
             if (data2 && data2.email === email) return this.error('邮箱已存在')
--- react/src/pages/login/SignIn.js
+++ react/src/pages/login/SignIn.js
@@ -3,29 +3,31 @@ import './index.scss'
 import NavHeader from 'public/NavHeader'
 class SignIn extends Component {
     state = {
-        check: false
+        check: false,
+        username: '',
+        password: ''
     }
     render() {
         return (
             <div className='public'>
                 <div className='sign-in-warp warp'></div>
                 <div className='header'>
-                    <NavHeader goBack={this.goBack} icon={true} title='登 录'/>
+                    <NavHeader goBack={this.goBack} icon={true} title='登 录' />
                 </div>
                 <div className='inputs'>
                     <div className='pwd'>
-                        <input maxLength='10' type='text' placeholder='USERNAME OR EMAIL' />
+                        <input maxLength='10' name='username' defaultValue={this.state.username} onChange={this.handleInputChange} type='text' placeholder='USERNAME OR EMAIL' />
                         <i className='fa fa-close' onClick={this.clear}></i>
                     </div>
                     <div className='pwd'>
-                        <input maxLength='16' type={this.state.check ? 'text' : 'password'} placeholder='PASSWORD' />
+                        <input maxLength='16' name='password' defaultValue={this.state.password} onChange={this.handleInputChange} type={this.state.check ? 'text' : 'password'} placeholder='PASSWORD' />
                         <i className={`fa ${this.state.check ? 'fa-eye' : 'fa-eye-slash'}`} onClick={this.checkEye}></i>
                     </div>
                     <p>
                         <span onClick={this.goRegister}>新账户</span>
                         <span onClick={this.restorePassword}>忘记密码？</span>
                     </p>
-                    <span className='btn'>登 录</span>
+                    <span className='btn' onClick={this.login}>登 录</span>
                 </div>
             </div>
         )
@@ -56,7 +58,18 @@ class SignIn extends Component {
     restorePassword = () => {
         this.props.history.push('/login/restorePassword');
     }
+
+    handleInputChange = e => {
+        e.persist()
+        this.setState(prev => ({
+            [e.target.name]: e.target.value
+        }))
+    }
+
+    // 登录
+    login = () => {
+        console.log(this.state.password);
+        console.log(this.state.username);
+    }
 }
-
-
 export default SignIn
